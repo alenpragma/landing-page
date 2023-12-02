1ebfafb546c1b14cccfda893d6a7978b91dc0ee5
HamzaWebPro <hamza.mern2201@gmail.com>
2023-12-02 12:15:59 +0600

    added

--- app/_components/_banner/Banner.jsx
+++ app/_components/_banner/Banner.jsx
@@ -30,28 +30,43 @@ const Banner = () => {
             {/* nav bar svg end */}
           </div>
           {/* banner start */}
-          <div className="w-full flex flex-col    gap-y-14 md:gap-x-5 relative z-3 mt-0 md:mt-12">
-                   <div className="text-center">
-                    <h1 className="text-[30px] md:text-[50px] font-bold text-main-green">
-                    অল্প খরচে দ্রুত প্রাথমিক রোগ নিরাময় সল্যুশন
-                    </h1>
-                    <h3 className="text-[14px] md:text-[20px] text-white">
-                    দ্রুত ও নির্ভরযোগ্য প্রাথমিক চিকিৎসা সল্যুশন হচ্ছে আমাদের এই ফাস্ট এইড বক্স
-                    </h3>
-                  
-                   </div>
-                   <div className="">
-                   <Image src={banneproduct} className="w-full h-auto " />
-                   </div>
+          <div className="w-full flex flex-col   gap-y-7  lg:gap-y-14 md:gap-x-5 relative z-3 mt-0 md:mt-12">
+            <div className="text-center">
+              <h1 className="text-[30px] md:text-[50px] font-bold text-main-green">
+                অল্প খরচে দ্রুত প্রাথমিক রোগ নিরাময় সল্যুশন
+              </h1>
+              <h3 className="text-[14px] md:text-[20px] text-white">
+                দ্রুত ও নির্ভরযোগ্য প্রাথমিক চিকিৎসা সল্যুশন হচ্ছে আমাদের এই
+                ফাস্ট এইড বক্স
+              </h3>
+            </div>
+            <div className="flex justify-center">
+              {/* <Image src={banneproduct} className="w-full h-auto " /> */}
+              <iframe
+                className="w-full md:w-[60%] h-[400px]"
+                src="https://www.youtube.com/embed/1Tn_tu3NDYg?si=OoDTFSz_TY1mfzMg"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              ></iframe>
+            </div>
           </div>
           {/* banner end */}
-            {/* button start */}
-            <div className="flex justify-center relative z-3 mt-7 md:mt-7">
-                    <Link href={"/"} className="w-[150px] text-center overflow-hidden py-[10px] bg-main-green hover:text-main-green duration-500 hover:bg-white text-white   rounded-lg flex items-center gap-x-2 group"><span className="ml-[40px] group-hover:ml-[30px] duration-300 justify-center">Buy Now</span> <MdShoppingCartCheckout className="mt-[2px] translate-x-[-300px] group-hover:translate-x-0 duration-300" /></Link>
+          {/* button start */}
+          <div className="flex justify-center relative z-3 mt-7 md:mt-14">
+            <Link
+              href={"/"}
+              className="w-[150px] text-center overflow-hidden py-[10px] bg-main-green hover:text-main-green duration-500 hover:bg-white text-white   rounded-lg flex items-center gap-x-2 group"
+            >
+              <span className="ml-[40px] group-hover:ml-[30px] duration-300 justify-center">
+                Buy Now
+              </span>{" "}
+              <MdShoppingCartCheckout className="mt-[2px] translate-x-[-300px] group-hover:translate-x-0 duration-300" />
+            </Link>
+          </div>
 
-                    </div>
-
-                    {/* button end */}
+          {/* button end */}
         </Container>
       </div>
     </>
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from './_components/_banner/Banner'
+import WhoCanUse from './_components/_whocanuse/page'
 
 const page = () => {
   return (
     <>
     <Banner/>
+    <WhoCanUse/>
     </>
   )
 }
