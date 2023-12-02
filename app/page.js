7fb27f03aba9500079ada539b44d6010cbf656d5
HamzaWebPro <hamza.mern2201@gmail.com>
2023-12-02 14:36:35 +0600

    why use added

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './_components/_banner/Banner'
 import WhoCanUse from './_components/_whocanuse/page'
+import WhyShould from './_components/_whyyoushouldbuy/page'
 
 const page = () => {
   return (
     <>
     <Banner/>
     <WhoCanUse/>
+    <WhyShould/>
     </>
   )
 }
