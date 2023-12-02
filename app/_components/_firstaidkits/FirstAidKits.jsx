15748ff2487c3d3d986ac60d1671ca2f3499407a
HamzaWebPro <hamza.mern2201@gmail.com>
2023-12-02 18:38:39 +0600

    added

---
+++ app/_components/_firstaidkits/FirstAidKits.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Container from "../_container/Container";
+import Image from "next/image";
+import aud from "../../_svg/aud.svg";
+import { TiTick } from "react-icons/ti";
+const FirstAidKits = () => {
+  let audienceData = [
+    "কটন, ব্যান্ডেজ",
+    "কমপ্রেস ব্যান্ডেজ",
+    "স্যানিটাইজিং ওয়াইপ",
+    "থার্মোমিটার, ড্যান্ড্রাফ শ্যাম্পু",
+    "টুইজারস, স্যাফটি পিন",
+    "প্যারাসিটামল (জ্বরের জন্য)",
+    "বাম (মাথা বেথার জন্য )",
+  ];
+  return (
+    <div className="py-[50px] md:py-[80px] bg-[#f6f6f6]">
+      <Container>
+        <h1 className="text-[30px] md:text-[50px] text-center font-bold text-main-dark-green">
+          ফাস্ট এইড বাক্সটিতে যা যা পাচ্ছেন ?
+        </h1>
+        <div className="flex flex-col items-center  md:flex-row gap-y-5 md:gap-x-5 mt-5 md:mt-10">
+          <div className="w-full md:w-[49%]">
+            <ul className="flex flex-col gap-y-5">
+              {audienceData.map((item) => (
+                <li className="text-[20px] md:text-[30px] text-main-green font-bold flex items-center gap-x-2">
+                  <TiTick className="text-[25px] text-main-dark-green" /> {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="w-full md:w-[49%]">
+            <Image src={aud} className="w-full h-auto" />
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default FirstAidKits;
--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from './_components/_banner/Banner'
 import WhoCanUse from './_components/_whocanuse/page'
 import WhyShould from './_components/_whyyoushouldbuy/page'
+import FirstAidKits from './_components/_firstaidkits/FirstAidKits'
 
 const page = () => {
   return (
@@ -9,6 +10,7 @@ const page = () => {
     <Banner/>
     <WhoCanUse/>
     <WhyShould/>
+    <FirstAidKits/>
     </>
   )
 }
